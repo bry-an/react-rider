905f6e57b92509a7126ae21b897874a2a4ae27ee
Bryan Yunis <bbyunis@gmail.com>
2018-11-02 13:59:07 -0600

    location entry form

--- client/src/Components/RouteEditor/RouteEditor.js
+++ client/src/Components/RouteEditor/RouteEditor.js
@@ -4,7 +4,13 @@ export default class RouteEditor extends Component {
   render() {
     return (
       <div>
-        
+        <form>
+          <input
+            type='text'
+            name='location-entry'
+          />
+        </form>
+
       </div>
     )
   }
